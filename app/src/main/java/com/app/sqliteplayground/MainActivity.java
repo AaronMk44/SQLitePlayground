65ca3ff4a598b26dc4a0f548f1531aed6473916e
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-08-20 23:46:27 +0200

    test: Tested Customer entity and model
    Added some test code to verify whether the Customer Model is
    working as well as the model.

--- app/src/main/java/com/app/sqliteplayground/MainActivity.java
+++ app/src/main/java/com/app/sqliteplayground/MainActivity.java
@@ -3,6 +3,9 @@ package com.app.sqliteplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ContactModel db = new ContactModel(this);
+
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact(1,"Ravi", "9100000000"));
+        db.addContact(new Contact(2,"Srinivas", "9199999999"));
+        db.addContact(new Contact(3,"Tommy", "9522222222"));
+        db.addContact(new Contact(4,"Karthik", "9533333333"));
+
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();
+
+        for (Contact cn : contacts) {
+            String log = "Id: " + cn.getId() + " ,Name: " + cn.getName() + " ,Phone: " +
+                    cn.getPhoneNumber();
+            // Writing Contacts to log
+            Log.d("Name: ", log);
+        }
     }
 }
